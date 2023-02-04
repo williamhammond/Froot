cdf1cb2be1ca4876ea58b0b10c31cae45bb633f8
WilliamHammond <william.t.hammond@gmail.com>
2023-02-04 14:27:29 -0500

    Adds raytrace to detect tile in front of player

--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Vector2=UnityEngine.Vector2;
@@ -9,6 +10,8 @@ namespace Player {
         
         private PlayerInput _playerInput;
         private InputAction _playerMove;
+        
+        private InputAction _playerAction;
 
         public void Awake () {
             _controller = gameObject.AddComponent<CharacterController>();
@@ -16,9 +19,33 @@ namespace Player {
             _playerInput = gameObject.GetComponent<PlayerInput>();
             _playerMove = _playerInput.actions["Move"];
         }
+
+        public void OnEnable () {
+            _playerAction = _playerInput.actions["Action"];
+            _playerAction.performed += HandlePlayerAction;
+        }
         
+        public void OnDisable () {
+            _playerAction.performed -= HandlePlayerAction;
+        }
+
         void Update() {
             HandleMovement();
+        }
+
+        private void HandlePlayerAction (InputAction.CallbackContext ctx) {
+            float maxRayDistance = 2f;
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Ray ray = new Ray(transform.position, forward);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, maxRayDistance, LayerMask.GetMask("Default")))
+            {
+                Debug.Log("Object detected directly in front of player: " + hitInfo.collider.gameObject.name);
+            }
+            else
+            {
+                Debug.Log("Nothing detected directly in front of player");
+            }
         }
 
         private void HandleMovement () {
