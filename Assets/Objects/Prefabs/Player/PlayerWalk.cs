5c608d434edcc09d1a4b6c7584e9bc22e4d3b482
WilliamHammond <william.t.hammond@gmail.com>
2023-02-05 14:02:01 -0500

    Maybe

--- Assets/Objects/Prefabs/Player/PlayerWalk.cs
+++ Assets/Objects/Prefabs/Player/PlayerWalk.cs
@@ -2,6 +2,7 @@ using System;
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Events;
 
