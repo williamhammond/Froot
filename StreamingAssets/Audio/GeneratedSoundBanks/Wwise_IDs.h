af8543d0910ebfdc7addc97b18ae619b56856c2e
PJRandol <60558135+PJRandol@users.noreply.github.com>
2023-02-05 18:04:52 +0000

    Deploy

---
+++ StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,78 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BREAKROCK = 3614189553U;
+        static const AkUniqueID BREATH = 1326786195U;
+        static const AkUniqueID COLLECTAIRCAN = 416902311U;
+        static const AkUniqueID COLLECTSOIL = 717635272U;
+        static const AkUniqueID COLLECTWATER = 1695098014U;
+        static const AkUniqueID GROWTILE = 1913626152U;
+        static const AkUniqueID HITROCK = 3660205123U;
+        static const AkUniqueID LOSETILE = 213112866U;
+        static const AkUniqueID PLAYFOOTSTEP = 1712852617U;
+        static const AkUniqueID PLAYWIND = 1592727253U;
+        static const AkUniqueID USEAIRCAN = 3282522952U;
+        static const AkUniqueID USEOXYGEN = 4290731854U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_SURFACE
+        {
+            static const AkUniqueID GROUP = 2720040714U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID ROCK = 2144363834U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace PLAYER_SURFACE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID OXYGEN = 3660512661U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BREATH = 1326786195U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PLAYER = 1069431850U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID OUTDOORREVERB = 3250445679U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
