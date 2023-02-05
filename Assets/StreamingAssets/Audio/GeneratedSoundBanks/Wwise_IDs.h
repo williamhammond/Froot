b2c7d4819b8f8cf0dcb74658bfa95273b0d1bcb5
Jackson Mumford <jmumf25@gmail.com>
2023-02-04 23:35:28 -0500

    SFX Update
    SFX Events for: PlayWind, BreakRock, CollectAirCan, CollectSoil, CollectWater, HitRock, UseAirCan, UseOxygen, PlayFootstep with Switch Player_Surface for different ground types, GrowTile, and Breath with RTPC Oxygen (0-100)

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,18 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID BREAKROCK = 3614189553U;
+        static const AkUniqueID BREATH = 1326786195U;
+        static const AkUniqueID COLLECTAIRCAN = 416902311U;
+        static const AkUniqueID COLLECTSOIL = 717635272U;
+        static const AkUniqueID COLLECTWATER = 1695098014U;
+        static const AkUniqueID GROWTILE = 1913626152U;
+        static const AkUniqueID HITROCK = 3660205123U;
+        static const AkUniqueID LOSETILE = 213112866U;
+        static const AkUniqueID PLAYFOOTSTEP = 1712852617U;
+        static const AkUniqueID PLAYWIND = 1592727253U;
+        static const AkUniqueID USEAIRCAN = 3282522952U;
+        static const AkUniqueID USEOXYGEN = 4290731854U;
     } // namespace EVENTS
 
     namespace SWITCHES
@@ -33,6 +44,11 @@ namespace AK
 
     } // namespace SWITCHES
 
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID OXYGEN = 3660512661U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -41,8 +57,15 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID BREATH = 1326786195U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PLAYER = 1069431850U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID OUTDOORREVERB = 3250445679U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
